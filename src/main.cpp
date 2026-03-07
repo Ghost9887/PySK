aac57939a27fee017d1186aae387695b03b4e4c1
Ghost <duchojakub987@gmail.com>
2026-03-07 21:16:50 +0100

    cmd args parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include "common.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << '\n';
+void run(const char *file_name) {
+    std::cout << "run" << '\n';
+}
+
+void repl() {
+    std::cout << "repl" << '\n';
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 2) {
+        std::cout << "Too many arguments" << '\n';
+        std::cout << "Usage: dio [file_name]" << '\n';
+    }else if (argc < 2){
+        repl();
+    }else {
+        run(argv[1]);
+    }
     return 0;
 }
