0ef4daea7892011cceaa60ddc3cf11213e76e386
Ghost <duchojakub987@gmail.com>
2026-03-10 09:57:23 +0100

    restart

--- include/common.hpp
+++ include/common.hpp
@@ -9,20 +9,4 @@
 #include <unordered_map>
 #include <variant>
 
-typedef enum {
-    EOS, PLUS, SUB, STAR, SLASH, 
-    R_PAREN, L_PAREN, R_BRACKET, L_BRACKET, 
-    COMMA, EQUALS, GREATER, LESSER, DOT, NOT,
-
-    GREATER_EQUALS, LESSER_EQUALS, EQUALS_EQUALS,
-    NOT_EQUALS,
-
-    IDENTIFIER, NUMBER, STRING, VYTLAC, KED, INAK, 
-    INAK_KED, AK, POKIAL, VRATIT,  FUNK,
-    TRIEDA
-}TokenType;
-
-extern std::unordered_map<TokenType, std::string> tokens_table;
-extern std::unordered_map<std::string, TokenType> keywords_table;
-
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,6 @@
 #include "common.hpp"
-#include "parser.hpp"
-
-void run(const char *file_name) {
-    Scanner scanner;
-    scanner.load(file_name);
-    scanner.tokenize();
-    scanner.print_tokens();
-
-    Parser parser(scanner.get_tokens());
-    parser.parse();
-}
-
-void run(std::string content) {
-    Scanner scanner(content);
-    scanner.tokenize();
-    scanner.print_tokens();
-}
-
-void repl() {
-    std::string input;
-    while (true) {
-        std::cout << "> ";
-        std::cin >> input;
-        if (input == "q" || input == "quit") break;
-        run(input);
-    }
-}
 
 int main(int argc, char *argv[]) {
-    if (argc > 2) {
-        std::cout << "Too many arguments" << '\n';
-        std::cout << "Usage: dio [file_name]" << '\n';
-    }else if (argc < 2){
-        repl();
-    }else {
-        run(argv[1]);
-    }
+    std::cout << "hello, world" << '\n';
     return 0;
 }
