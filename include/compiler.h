bc74f71d8cdb9881b366ec0ba1ca7ab6098ba9f4
Ghost <duchojakub987@gmail.com>
2026-03-24 10:56:29 +0100

    refactor

--- include/compiler.h
+++ include/compiler.h
@@ -17,6 +17,7 @@ private:
     void evaluate_expression(std::shared_ptr<Expr> expr);
     void evaluate_binary_expression(std::shared_ptr<BinaryExpr> expr);
     LiteralValue get_value(std::shared_ptr<Expr> expr);
+    void emit_binary_expr(Value value1, Value value2, Byte byte, int line);
     int get_line(Token &token);
     void emit_byte(Byte byte, int line);
     void emit_value(Value value, int line);
--- src/compiler.cc
+++ src/compiler.cc
@@ -30,46 +30,40 @@ void Compiler::evaluate_expression(std::shared_ptr<Expr> expr) {
 void Compiler::evaluate_binary_expression(std::shared_ptr<BinaryExpr> expr) {
     LiteralValue a = get_value(expr->left);
     LiteralValue b = get_value(expr->right);
+    Byte code;
 
     switch (expr->op.type) {
         case T_PLUS: {
             //double
             if (std::holds_alternative<Value>(a) && std::holds_alternative<Value>(b)) {
-                emit_value(std::get<Value>(a), get_line(expr->op));
-                emit_value(std::get<Value>(b), get_line(expr->op));
-                emit_byte(OP_ADD, get_line(expr->op));
+                code = OP_ADD;
             }
             break;
         }
         case T_MINUS: {
             //double
             if (std::holds_alternative<Value>(a) && std::holds_alternative<Value>(b)) {
-                emit_value(std::get<Value>(a), get_line(expr->op));
-                emit_value(std::get<Value>(b), get_line(expr->op));
-                emit_byte(OP_MINUS, get_line(expr->op));
+                code = OP_MINUS;
             }
             break;
         }
         case T_STAR: {
             //double
             if (std::holds_alternative<Value>(a) && std::holds_alternative<Value>(b)) {
-                emit_value(std::get<Value>(a), get_line(expr->op));
-                emit_value(std::get<Value>(b), get_line(expr->op));
-                emit_byte(OP_MULTIPLY, get_line(expr->op));
+                code = OP_MULTIPLY;
             }
             break;
         }
         case T_SLASH: {
             //double
             if (std::holds_alternative<Value>(a) && std::holds_alternative<Value>(b)) {
-                emit_value(std::get<Value>(a), get_line(expr->op));
-                emit_value(std::get<Value>(b), get_line(expr->op));
-                emit_byte(OP_DIVIDE, get_line(expr->op));
+                code = OP_DIVIDE;
             }
             break;
         }
         defualt: break;
     }
+    emit_binary_expr(std::get<Value>(a), std::get<Value>(b), code, get_line(expr->op));
 }
 
 LiteralValue Compiler::get_value(std::shared_ptr<Expr> expr) {
@@ -82,6 +76,12 @@ LiteralValue Compiler::get_value(std::shared_ptr<Expr> expr) {
 
 int Compiler::get_line(Token &token) {
     return token.line;
+}
+
+void Compiler::emit_binary_expr(Value value1, Value value2, Byte byte, int line) {
+    emit_value(value1, line);
+    emit_value(value2, line);
+    emit_byte(byte, line);
 }
 
 void Compiler::emit_byte(Byte byte, int line) {
